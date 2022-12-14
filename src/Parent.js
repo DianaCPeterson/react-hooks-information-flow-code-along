ab409930dd0e854de69f5aca788ffce0626846f1
Diana C Peterson <dpeter10@gmail.com>
2022-12-14 09:30:37 -0500

    commit tree 13ac89426f895ee47367ab5fa557908ec8feffb4

--- src/Parent.js
+++ src/Parent.js
@@ -6,11 +6,22 @@ function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
 
+  function handleChangeColor() {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+  }
+  
+  function Child({ onChangeColor }) {
+    console.log(onChangeColor);
+    return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+  }
+
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={handleChangeColor}/>
+      <Child onChangeColor={handleChangeColor} />
     </div>
+  
   );
 }
 
